d449bd47d79fa7c8f93661ada782a4248815afc4
Jonas Luz Jr. <github@jonasluz.com.br>
2025-12-25 21:51:02 -0300

    Lab Results | Analysis of m5 family for 500 users.

--- Project-4/scripts/run_batch.ps1
+++ Project-4/scripts/run_batch.ps1
@@ -8,11 +8,9 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 # Define Experiments
 $experiments = @(
-    @{ Name = 't3-small_500u_24i'; Count = 24; Type = 't3.small'; Users = 500; Duration = '3m' },
-    @{ Name = 't3-medium_500u_12i'; Count = 12; Type = 't3.medium'; Users = 500; Duration = '3m' },
-    @{ Name = 't3-large_500u_6i'; Count = 6; Type = 't3.large'; Users = 500; Duration = '3m' },
-    @{ Name = 't3-xlarge_500u_3i'; Count = 3; Type = 't3.xlarge'; Users = 500; Duration = '3m' },
-    @{ Name = 't3-2xlarge_500u_1i'; Count = 1; Type = 't3.2xlarge'; Users = 500; Duration = '3m' }
+    @{ Name = 'm5-large_500u_5i'; Count = 5; Type = 'm5.large'; Users = 500; Duration = '3m' },
+    @{ Name = 'm5-xlarge_500u_2i'; Count = 2; Type = 'm5.xlarge'; Users = 500; Duration = '3m' },
+    @{ Name = 'm5-2xlarge_500u_1i'; Count = 1; Type = 'm5.2xlarge'; Users = 500; Duration = '3m' }
 )
 
 foreach ($exp in $experiments) {
